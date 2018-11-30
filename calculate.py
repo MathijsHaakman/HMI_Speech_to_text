bcdde24cb7502bdaf4a8e6076849e9caa28099c2
Evert0x <evert@hatching.io>
2018-11-30 15:10:30 +0100

    added wrr and wer

--- calculate.py
+++ calculate.py
@@ -1,4 +1,25 @@
 import os
+import requests
+
+with open(os.path.join("Results", "results.csv"), 'r') as list:
+    list.readline()
+    line = list.readline()
+    print("number;wrr,wer")
+    while line:
+        data = line.split(";")
+        reference = data[1]
+        guess = data[3]
+
+        r = requests.post(
+            "http://api.robbit.me",
+            json={
+                "guess": guess,
+                "reference": reference
+            }
+        ).json()
+
+        print("{};{};{}".format(data[0], r["wrr"], r["wer"]))
 
 
-with open(os.path.join("Results", "result.csv"), 'r') as list:
+
+        line = list.readline()
