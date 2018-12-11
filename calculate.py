f0c8502ca1e524f1754817e51ba3d09998b728f1
Mathijs Haakman <MathijsHaakman@outlook.com>
2018-12-12 00:06:40 +0100

    added methods for precision and recall calculations

--- calculate.py
+++ calculate.py
@@ -1,5 +1,36 @@
 import os
 import requests
+import re
+
+
+def calculate_precision_and_recall(reference: str, guess: str):
+    regex = re.compile('\.')
+    reference_list = regex.sub('', reference.lower()).split(' ')
+    guess_list = regex.sub('', guess.lower()).split(' ')
+    unique_list = set(reference_list)
+    for word in list:
+        print(word)
+
+
+def get_precision(value: str, reference: list, guess: list):
+    correct = 0
+    total = 0
+    for index, word in enumerate(guess):
+        if word == value:
+            total += 1
+            if index < len(reference) & reference[index] == value:
+                correct += 1
+
+
+def get_recall(value: str, reference: list, guess: list):
+    correct = 0
+    total = 0
+    for index, word in enumerate(reference):
+        if word == value:
+            total += 1
+            if index < len(guess) & guess[index] == value:
+                correct += 1
+
 
 with open(os.path.join("Results", "results.csv"), 'r') as list:
     list.readline()
